8d69af88cdf7ae176e1e4d36bee72f8e9f8aa8ec
Charlie Mayer <cmayerproductions@gmail.com>
2016-11-28 22:06:12 -0500

    update

--- new.js
+++ new.js
@@ -1,53 +1,55 @@
-$(document).ready(function() {
-  /** function and created variables for elements within NY Times result object **/
-  function create(result){
-    var web_url=result.results[0].url;
-    var multimedia= result.results[0].multimedia[4].url;
-    var snippet=result.results[0].abstract;
-}
-/** select  **/
-  $('select').on( "change", function() {
-    $('header').animate({"width":"auto"});
+$(document).ready(function(){
+    $("select").on("change", function(e){
+        e.preventDefault();
 
+        // CREATE THE ARTICLES
+        function buildArticle (result) {
+            $("article").not(':first').remove();
+            var articleCounter = 1;
+            for (var i in result.results) {
+                if (result.results[i].multimedia.length != 0) {
+                    if (articleCounter < 13) {
+                        var webUrl = result.results[i].url;
+                        var introInfo = result.results[i].abstract;
+                        var image = result.results[i].multimedia[4].url;
+                        var clones = $("article").eq(0).clone();
 
-    var value= $('select option:selected').val();
-    var url = "https://api.nytimes.com/svc/topstories/v2/" + value +  '.json';
-    url += '?' + $.param({
-      'api-key': "2824c92828de454daccd0f86c22fb3e4"
-  });
+                        $(clones).css('background-image', "url("+ image + ")");
+                        $(clones).css('background-size', "cover");
+                        $(clones).children("a").children(".space-holder").children(".blurb").children("p").text(introInfo);
+                        $(clones).children("a").attr("href", webUrl);
 
- /** get articles from NY Times API**/
-    $.ajax({
-      url: url,
-      method: 'GET',
-    }).done(function(result) {
-        create(result);
-        var articles= result.results;
-        for (var i=0; i < articles.length; i++) {
+                        $("article").show();
+                        $(".loader").hide();
+                        $(".results").append(clones);
+                        articleCounter = articleCounter +1;
+                        }
+                    }
+                }
+                $("article").eq(0).hide();
+        }
 
-            if(result.results[i].multimedia.length>0) {
-                var clone=$('article').eq(0).clone();
-                var web_url=result.results[i].url;
-                var multimedia=  result.results[i].multimedia[4].url;
-                var snippet=result.results[i].abstract;
-            /**using clone to duplicate elements  **/
-                $(clone).css("background-image", "url("+ multimedia +")");
-                $(clone).children('.blurb').html(snippet);
-                $(clone).children('.url').attr("href", web_url);
+        // ANIMATE THE HEADER MOVEMENT
+        $("header.flex").css("height", "auto");
+        $(".logo").animate({"width":"130px"}, 400);
+        $("img").animate({"margin":"0 3em"}, 400);
 
-            /**appending to  div with results class**/
-                $('.results').append(clone);
-                $('.article').show();
-
-              }
-
-          }
-       /**hide blank article  **/
-          $("article").eq(0).hide();
-    }).fail(function(err) {
-      throw err;
-    });
-
-
-  })
+        // AJAX CALL TO GET THE ARTICLES
+        var name = $('select option:selected').val();
+        console.log(name);
+        var url = "https://api.nytimes.com/svc/topstories/v2/" + name+".json";
+        url += '?' + $.param({
+          'api-key': "2824c92828de454daccd0f86c22fb3e4"
+        });
+        $.ajax({
+          url: url,
+          method: 'GET',
+        }).done(function(result) {
+            $(".loader").show();
+          console.log(result);
+            buildArticle(result);
+        }).fail(function(err) {
+          throw err;
+        });
+    })
 })
